7b0dbb4c15c4173ef9806f9d974d866c0b210f5c
ishan-surana <ishan5.mitmpl2022@learner.manipal.edu>
2024-04-23 18:50:30 +0530

    Scroller and Trigger added

--- maintainance.py
+++ maintainance.py
@@ -11,11 +11,11 @@ def maintainance():
         conn = sqlite3.connect('classroom_allotment_system.db')
         cursor = conn.cursor()
         cursor.execute("DELETE FROM status WHERE EXISTS (SELECT 1 FROM requests WHERE requests.request_id = status.request_id AND ((requests.date = ? AND requests.end_time <= ?) OR (requests.date < ?)));", (current_date, current_time, current_date))
-	cursor.execute("DELETE FROM slots WHERE EXISTS (SELECT 1 FROM requests WHERE requests.request_id = slots.request_id AND ((requests.date = ? AND requests.end_time <= ?) OR (requests.date < ?)));", (current_date, current_time, current_date))
+        cursor.execute("DELETE FROM slots WHERE EXISTS (SELECT 1 FROM requests WHERE requests.request_id = slots.request_id AND ((requests.date = ? AND requests.end_time <= ?) OR (requests.date < ?)));", (current_date, current_time, current_date))
         cursor.execute("DELETE FROM requests WHERE ((requests.date = ? AND requests.end_time <= ?) OR (requests.date < ?));", (current_date, current_time, current_date, ))
         cursor.close()
         conn.commit()
-        time.sleep(86,400) #daily deletion
+        time.sleep(86400) #daily deletion
 
 maintainance_thread = threading.Thread(target=maintainance)
 maintainance_thread.start()
